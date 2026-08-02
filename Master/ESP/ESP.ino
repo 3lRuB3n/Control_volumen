9eceaaa56a0d1333a1998a8ef49ff895ea0209fe
3lRuB3n <rubengarridolagares1@Gmail.com>
2026-08-02 23:56:36 +0200

    Update ESP.ino
    Actualizado el método para indicar el numero de receptores.
    Necesario arreglo de la línea 27, posiblemente necesario rehacer como se manejan las direcciones

--- Master/ESP/ESP.ino
+++ Master/ESP/ESP.ino
@@ -3,12 +3,8 @@
 
 #define println Serial.println      //habría que cambiar los println por print, o recortar los mensajes que mande en el receptor (La raspberrry pi pico, con circuitpython)
 
-byte dir[20][6] //20 direcciones máximas (cosas de esp_now) y 6 bytes (ordenados así por cosas de las ip)
-esp_now_peer_info_t peerInfo;
-
-byte onDataSent(byte *mac_addr byte sendStatus){
-  println(sendStatus);
-} 
+byte dir[20][6]; //20 direcciones máximas (cosas de esp_now) y 6 bytes (ordenados así por cosas de las ip)
+//esp_now_peer_info_t peerInfo;
 
 void setup(){
   Serial.begin(115200);
@@ -25,27 +21,28 @@ void setup(){
 
 void loop(){
   String data = leer();
-  int obj = cahr2hex data.substring(0,2); //quizas sea 1 en vez de 2?
+  int obj = letraA20(data[1]);
   String mensaje = data.substring(2);
-  
-  esp_now_sen(dir[2][], mensaje, sizeof(mensaje)); //mete esto en un do while, puede que lo de la dirección no le haga gracia a la funcion esta
+  //abajo toca hacer cositas con los punteros, de momento no se que le pasa a esto, mejor miralo en un compilador online de c y luego intenta implementarlo
+  esp_now_send(dir + (sizeof)*(obj), mensaje, sizeof(mensaje)); //mete esto en un do while, puede que lo de la dirección no le haga gracia a la funcion esta
 }
 
 void addPeers(){
-   int N_rec = char2hex(leer())
+   int N_rec = letraA20(leer()[0]); //a=0, b=1, c=2 ... u=20
    if(N_rec == 0){
     println("Apagando");
     //ESP.deepSleep(0); //Se pone en stand-by hasta que se reinicie
    }
-   for(int j=0;j<N_rec;j++){
+   for(int j = 0;j<N_rec;j++){
     for(int i=0;i<11;i+=2){
+      String dir_char = leer(); //lee la siguiente dirección
       dir[j][i/2] = char2hex(dir_char[i])*16 + char2hex(dir_char[i+1]);
     }
    }
-  println("anadidos");
+  println("anhadidos");
 }
 
-String lee(){ //leer el puerto serie comprimido a una sola funcion. Puede que sobre el delay o que sea excesivo (seguramente)
+String leer(){ //leer el puerto serie comprimido a una sola funcion. Puede que sobre el delay o que sea excesivo (seguramente)
   while(!Serial.available()){
     delay(100);
   }
@@ -61,3 +58,11 @@ byte char2hex(char letra){
     return letra - 'A' + 10;
   else return 0;
 }
+
+byte letraA20(char letra){
+  return letra - 'a';
+}
+
+/*byte onDataSent(byte *mac_addr byte, sendStatus){
+  println(sendStatus);
+} */
