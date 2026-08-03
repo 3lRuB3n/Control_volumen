6dbd25ce3b1717a3254c3f17e82236d87bbc0b13
3lRuB3n <rubengarridolagares1@Gmail.com>
2026-08-03 15:56:07 +0200

    Update ESP.ino
    Aparentemente funcional, a la espera de tener un receptor y programar la parte de la raspberry pi pico

--- Master/ESP/ESP.ino
+++ Master/ESP/ESP.ino
@@ -3,51 +3,51 @@
 
 #define println Serial.println      //habría que cambiar los println por print, o recortar los mensajes que mande en el receptor (La raspberrry pi pico, con circuitpython)
 
-byte dir[20][6]; //20 direcciones máximas (cosas de esp_now) y 6 bytes (ordenados así por cosas de las ip)
-//esp_now_peer_info_t peerInfo;
+unsigned char dir[20*6]; //20 direcciones máximas (cosas de esp_now) y 6 bytes (ordenados así por cosas de las ip)
 
 void setup(){
   Serial.begin(115200);
-  //WiFi.mode(WIFI_STA);
-
-  /*if (esp_now_init() != ESP_OK) return; 
-  Serial.println("ok");
+  WiFi.mode(WIFI_STA);
+  println("on");
+  if (esp_now_init() != 0) return; 
+  println("ok");
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-  esp_now_register_send_cb(OnDataSent);*/
+  esp_now_register_send_cb(onDataSent);
 
   addPeers();
 }
 
 void loop(){
   String data = leer();
-  int obj = letraA20(data[1]);
-  String mensaje = data.substring(2);
-  //abajo toca hacer cositas con los punteros, de momento no se que le pasa a esto, mejor miralo en un compilador online de c y luego intenta implementarlo
-  esp_now_send(dir + (sizeof)*(obj), mensaje, sizeof(mensaje)); //mete esto en un do while, puede que lo de la dirección no le haga gracia a la funcion esta
+  unsigned char obj = letraA20(data[0]);
+  String mensaje = data.substring(1);
+  
+  esp_now_send(dir+obj*6, (uint8_t *) &mensaje, sizeof(mensaje)); //mete esto en un do while, puede que lo de la dirección no le haga gracia a la funcion esta
 }
 
 void addPeers(){
    int N_rec = letraA20(leer()[0]); //a=0, b=1, c=2 ... u=20
    if(N_rec == 0){
-    println("Apagando");
-    //ESP.deepSleep(0); //Se pone en stand-by hasta que se reinicie
+    ESP.deepSleep(0); //Se pone en stand-by hasta que se reinicie
    }
    for(int j = 0;j<N_rec;j++){
+    String dir_char = leer(); //lee la siguiente dirección
     for(int i=0;i<11;i+=2){
-      String dir_char = leer(); //lee la siguiente dirección
-      dir[j][i/2] = char2hex(dir_char[i])*16 + char2hex(dir_char[i+1]);
+      dir[j*6+i/2] = char2hex(dir_char[i])*16 + char2hex(dir_char[i+1]);
     }
    }
   println("anhadidos");
 }
 
 String leer(){ //leer el puerto serie comprimido a una sola funcion. Puede que sobre el delay o que sea excesivo (seguramente)
+  //println("escuchando");
   while(!Serial.available()){
     delay(100);
   }
   String dato = Serial.readString();
   dato.trim();
+  //println(dato);
   return dato;
 }
 
@@ -60,9 +60,10 @@ byte char2hex(char letra){
 }
 
 byte letraA20(char letra){
+  //println(letra - 'a');
   return letra - 'a';
 }
 
-/*byte onDataSent(byte *mac_addr byte, sendStatus){
+void onDataSent(byte *mac_addr, byte sendStatus){
   println(sendStatus);
-} */
+}
