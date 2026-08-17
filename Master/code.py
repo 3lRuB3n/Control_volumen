c676e937d8ebb1085e9f24b6f7433286b78aee29
3lRuB3n <rubengarridolagares1@Gmail.com>
2026-08-17 23:27:45 +0200

    Update code.py
    Ahora la pantalla se apaga a los 30s de inactividad. La primera interacción que se tenga con el dispositivo servirá para despertarlo. Puede que sea mejor quitar eso pero de momento se queda

--- Master/code.py
+++ Master/code.py
@@ -1,8 +1,13 @@
 import esp, even, recep as rec, ControlDisplay as CD
 from time import sleep as sl
+from time import monotonic as t_act
 error = 0
 disp = 0
 ind = 0
+
+t_espera = 30 #seg
+t_even = t_act()
+on = True
 
 #aqui van los receptores con la direccion fisica y la mac
 disp = rec.recep("test", "0",0, 'u'), rec.recep("USB","0",1,'u')#, rec.recep("ESPNow","40915151EDA2",3,'a')
@@ -10,12 +15,17 @@ Ndisp = len(disp)
 CD.g_rec[0] = disp[ind].img
 #esp.setup(disp)
 
-while error == 0:
+while True:
+    if t_act() > t_even + t_espera:
+        on = False
+        
+    CD.marco.hidden = not on
     sl(0.01)
     pend, comandos = even.actu(even.pines)
     rotacion = even.act_inc(even.encoder)
     CD.apaga()
-    if pend:
+    if pend and on: #Todo lo que tenga que ver con botones
+        t_event = t_act()
         for i in comandos:
             if i == "sil":
                 CD.g_altavoz[0] = 2
@@ -25,8 +35,13 @@ while error == 0:
                 CD.g_rec[0] = disp[ind].img #cambia la imagen del receptor a la del dispositivo de turno
             else:
                 disp[ind].envia(i)
-    if rotacion != 0:
+    if rotacion != 0 and on:
+        t_event = t_act()
         CD.g_altavoz[0] = 0 if rotacion>0 else 1
         CD.ilumina("altavoz")
         rec.envia_audio(rotacion, disp[ind])
         CD.g_altavoz.hidden = False
+        
+    if (rotacion != 0 or pend) and not on:
+        on = True
+        t_even = t_act()
